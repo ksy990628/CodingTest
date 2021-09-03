48dd9fdf2383db845b2d9cc3b997d5b9ce14656e
김서영 <55133538+ksy990628@users.noreply.github.com>
2021-09-03 21:07:50 +0900

    Add files via upload

---
+++ 1주차_위클리챌린지.py
@@ -0,0 +1,10 @@
+def solution(price, money, count):
+    sum = 0
+    for i in range(count):
+        sum += price*(i+1)
+    
+    answer = sum - money
+    if answer <= 0:
+        answer = 0
+    
+    return answer
