85266ed072c47e59a9d5d2b5896143aec1ee2f0a
김서영 <55133538+ksy990628@users.noreply.github.com>
2021-12-20 22:59:48 +0900

    Create 3진법 뒤집기.java

---
+++ programmers/3진법 뒤집기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int solution(int n) {
+        int answer = 0;
+        int count = 0;
+        int standard = 1;
+        int a = n;
+        while (a >= 3){
+            a /= 3;
+            count += 1;
+            standard *= 3;
+        }
+        a = n;
+        for (int i=0; i<count+1; i++){
+            answer += (a % 3)*standard;
+            a /= 3;
+            standard /= 3;
+        }
+        return answer;
+    }
+}
