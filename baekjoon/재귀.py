e506554fba9b7a7342903347b58eb0286169aabb
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-02-11 22:45:40 -0800

    Update 재귀.py

--- baekjoon/재귀.py
+++ baekjoon/재귀.py
@@ -7,9 +7,22 @@ def factorial(n):
     else:
         return n * factorial(n-1)
     
-    
-
 print(factorial(n))
-2	  10870	  피보나치 수 
-3	  2447	  별 찍기 - 10
-4	  11729	  하노이 탑 이동 순서		
+
+#2	  10870	  피보나치 수 
+n = int(input())
+
+def fibo(n):
+    if n == 1:
+        return 1
+    elif n == 0:
+        return 0
+    else:
+        return fibo(n-1)+fibo(n-2)
+  
+print(fibo(n))
+
+#3	  2447	  별 찍기 - 10
+
+
+#4	  11729	  하노이 탑 이동 순서		
