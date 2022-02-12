92757e4725efcdab984cc6b4b81e1f9bba6cce38
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-02-11 19:49:22 -0800

    Create 재귀.py

---
+++ baekjoon/재귀.py
@@ -0,0 +1,15 @@
+#1	  10872	  팩토리얼
+n = int(input())
+
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+    
+
+print(factorial(n))
+2	  10870	  피보나치 수 
+3	  2447	  별 찍기 - 10
+4	  11729	  하노이 탑 이동 순서		
