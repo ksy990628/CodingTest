64166b276f8e6a4017dac06db9bac050d19e1cd5
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-03-23 23:21:26 +0900

    Update 11725.py

--- KBS/Tree/11725.py
+++ KBS/Tree/11725.py
@@ -1,25 +1,32 @@
-# 뭐야 개 어려움
-import sys 
-sys.setrecursionlimit(10**6) 
+# BFS -> Queue 이용
+# DFS -> 재귀 이용
 
-n = int(sys.stdin.readline()) 
-graph = [[] for i in range(n+1)] 
-for i in range(n-1): 
-  a, b = map(int, sys.stdin.readline().split()) 
-  graph[a].append(b) 
-  graph[b].append(a) 
-  
-  visited = [0]*(n+1) 
-  arr = [] 
-  
-  
-def dfs(s): 
-  for i in graph[s]: 
-    if visited[i] == 0: 
-      visited[i] = s 
-      dfs(i)
-      
-      
-dfs(1) 
-for x in range(2, n+1): 
-  print(visited[x])
+import sys
+from collections import deque
+
+
+N = int(sys.stdin.readline())
+graph = [[] for i in range(N+1)]
+for _ in range(N-1):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+    queue = deque()
+    queue.append(1)
+
+    ans = [0]*(N+1)
+
+def bfs():
+    while queue:
+        now = queue.popleft()
+        for nxt in graph[now]:
+            if ans[nxt] == 0:
+                ans[nxt] = now
+                queue.append(nxt)
+
+bfs()
+res = ans[2:]
+for x in res:
+    print(x)
+
