11c428ef5d77206829230acd20ffaec5d2e2eb74
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-03-30 20:01:50 +0900

    Update 15724.py

--- KBS/Dynamic Programming2/15724.py
+++ KBS/Dynamic Programming2/15724.py
@@ -1,3 +1,4 @@
+# 정답
 import sys
 input = sys.stdin.readline
 
@@ -12,3 +13,23 @@ for i in range(1,n+1):
 for _ in range(int(input())):
     x,y,i,j = map(int,input().split())
     print(dp[i][j] - dp[x-1][j] - dp[i][y-1] + dp[x-1][y-1])
+
+# 시간초과난 내 코드...
+import sys
+n, m = map(int,sys.stdin.readline().strip().split())
+target = []
+
+for i in range(n):
+    #print(i)
+    ipt = list(map(int,sys.stdin.readline().rstrip().split()))
+    #print(ipt)
+    target.append(ipt)
+
+k = int(sys.stdin.readline().strip())
+for _ in range(k):
+    result = 0
+    ipt = list(map(int,sys.stdin.readline().rstrip().split()))
+    for i in range(0,ipt[2],ipt[0]):
+        for j in range(0,ipt[3],ipt[1]):
+            result += target[i][j]
+    print(result)
