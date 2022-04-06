63fcf010ed8474c79a61be24d0435aaabfcdf104
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-04-06 19:20:18 +0900

    Create 1010.py

---
+++ KBS/Dynamic Programming1/1010.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    num = 1
+    for i in range(1,n+1):
+        num *= i
+    return num
+
+import sys
+t = int(sys.stdin.readline().strip())
+
+for _ in range(t):
+    n, m = map(int,sys.stdin.readline().rstrip().split())
+    bridge = factorial(m) // (factorial(n)*factorial(m-n))
+    print(bridge)
+   
