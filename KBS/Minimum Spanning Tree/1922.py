36a09bf210e21e62babec454423b99a11bed9974
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-05-09 00:02:26 +0900

    Update 1922.py

--- KBS/Minimum Spanning Tree/1922.py
+++ KBS/Minimum Spanning Tree/1922.py
@@ -1,1 +1,27 @@
-h
+import sys
+import heapq
+V = int(sys.stdin.readline().rstrip())
+E = int(sys.stdin.readline().rstrip())
+visited = [False]*(V+1)
+Elist = [[] for _ in range(V+1)]
+heap = [[0, 1]]
+
+for _ in range(E):
+    s, e, w = map(int,sys.stdin.readline().split())
+    Elist[s].append([w, e])
+    Elist[e].append([w, s])
+ 
+answer = 0
+cnt = 0
+while heap:
+    if cnt == V:
+        break
+    w, s = heapq.heappop(heap)
+    if not visited[s]:
+        visited[s] = True
+        answer += w
+        cnt += 1
+        for i in Elist[s]:
+            heapq.heappush(heap, i)
+ 
+print(answer)
