4879d5b78404c9f52263de3296b98852b078f871
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-05-16 22:52:48 +0900

    Create 15681.py

---
+++ KBS/Dynamic Programming On Trees/15681.py
@@ -0,0 +1,25 @@
+def countTree(x):
+    count[x]=1
+    for i in arr[x]:
+        if not count[i]:
+            countTree(i)
+            count[x]+=count[i]
+        
+
+
+import sys
+n,r,q = map(int,sys.stdin.readline().rstrip().split())
+arr = [[]for _ in range(n+1)]
+count = [0]*(n+1)
+result = 0
+
+for _ in range(n-1):
+    a, b = map(int,sys.stdin.readline().rstrip().split())
+    arr[a].append(b)
+    arr[b].append(a)
+
+countTree(r)
+
+for _ in range(q):
+    c = int(sys.stdin.readline().rstrip())
+    print(count[c])
