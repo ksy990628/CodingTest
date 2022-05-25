837c714f90e5a1b350b9559d991c4e5f9ddaf27e
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-05-25 22:27:59 +0900

    Update 2533.py

--- KBS/Dynamic Programming On Trees/2533.py
+++ KBS/Dynamic Programming On Trees/2533.py
@@ -2,7 +2,7 @@ import sys
 sys.setrecursionlimit(10 ** 9)
 n = int(sys.stdin.readline())
 c = [[] for i in range(n+1)]
-dp = [[0,0] for i in range(n+1)]
+dp = [[0,0] for i in range(n+1)] # 연결된 부분 트리의 얼리어답터의 수
 
 for _ in range(n-1):
     a,b = map(int , sys.stdin.readline().split(" "))
@@ -10,20 +10,30 @@ for _ in range(n-1):
     c[b].append(a)
 
 visited = [0 for i in range(n+1)]
+
+
+
 def dfs(start):
     global c
     global visited
     visited[start] = 1
     if len(c[start]) == 0:
+        # 부모가 얼리어답터인 경우
         dp[start][1] = 1
+        # 자식이 얼리어답터인 경우
         dp[start][0] = 0
     else:
         for i in c[start]:
             if visited[i] == 0:
                 dfs(i)
+                # 부모가 얼리어답터인 경우, 최소 얼리어답터의 수?
                 dp[start][1] += min(dp[i][0] , dp[i][1])
                 dp[start][0] += dp[i][1]
         dp[start][1] += 1
 
+        
+# root => 1
 dfs(1)
+
+# 최소 얼리어답터
 print(min(dp[1][0],dp[1][1]))
