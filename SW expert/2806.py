2652008f3160cd8a76e22fdbbc7e21c4d9c30c04
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-05-25 20:55:49 +0900

    Create 2806.py

---
+++ SW expert/2806.py
@@ -0,0 +1,31 @@
+def dfs(queen, n, row):
+    count = 0
+
+    # 마지막 행인지 확인
+    if n == row:
+        return 1
+
+    # 가로 자리 선정
+    for col in range(n):
+        queen[row] = col
+
+        for x in range(row):
+            # 세로로 겹치면 안됨
+            if queen[x] == queen[row]:
+                break
+
+            # 대각선으로 겹치면 안됨
+            if abs(queen[x] - queen[row]) == (row - x):
+                break
+        else:
+            count += dfs(queen, n, row + 1)
+
+    return count
+
+if __name__ == '__main__':
+    t = int(input())
+    for r in range(1, t+1):
+        n = int(input())
+        nQueen = [0 for i in range(n)]
+        res = dfs(nQueen,n,0)
+        print("#"+str(r)+" "+str(res))
