89995dbbb055d07bc35faaf946b4b9cc5b4d88a8
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-06-08 21:54:51 +0900

    Create 1068.py

---
+++ KBS/Tree/1068.py
@@ -0,0 +1,22 @@
+# 트리
+import sys
+
+def dfs(removed_node):
+    global arr
+    arr[removed_node] = -2
+    # 부모 노드가 누구인지가 해당 노드의 값이므로, 부모 노드만 찾아서 없애버리면 됨
+    for i in range(len(arr)):
+        if arr[i] == removed_node:
+            dfs(i)
+
+N = int(sys.stdin.readline().strip())
+arr = list(map(int, sys.stdin.readline().strip().split()))
+removed_node = int(sys.stdin.readline().strip())
+leaf_node_num = 0
+
+dfs(removed_node)
+
+for i in range(len(arr)):
+    if arr[i] != -2 and i not in arr:
+        leaf_node_num += 1
+print(leaf_node_num)
