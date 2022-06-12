5107f10dcd132937e19eac2b9df656a859e1638a
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-06-12 23:43:56 +0900

    Update 13305.py

--- KBS/Greedy/13305.py
+++ KBS/Greedy/13305.py
@@ -4,8 +4,10 @@ gas = list(map(int,input().split()))
 total = d[0]*gas[0]
 min_gas = gas[0]
 
+# 가격이 제일 싼 경우
 if min(gas) == gas[0]:
     total += gas[0]*(sum(d)-d[0])
+
 else: # min(gas) !=d[0]
     for i in range(1,len(gas)-1):
         if gas[i] < min_gas:
