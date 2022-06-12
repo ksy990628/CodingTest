ee1fd41f0a9809640d7ebc2256872922c173c1b9
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-06-12 23:42:19 +0900

    Create 13305.py

---
+++ KBS/Greedy/13305.py
@@ -0,0 +1,15 @@
+n = int(input())
+d = list(map(int,input().split()))
+gas = list(map(int,input().split()))
+total = d[0]*gas[0]
+min_gas = gas[0]
+
+if min(gas) == gas[0]:
+    total += gas[0]*(sum(d)-d[0])
+else: # min(gas) !=d[0]
+    for i in range(1,len(gas)-1):
+        if gas[i] < min_gas:
+            min_gas = gas[i]
+        total += min_gas*d[i]
+
+print(total)
