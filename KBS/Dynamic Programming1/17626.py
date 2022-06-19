6b08bb537373b964efb575fda879bf9d37bc8b67
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-06-19 23:46:35 +0900

    Create 17626.py

---
+++ KBS/Dynamic Programming1/17626.py
@@ -0,0 +1,15 @@
+import math
+import sys
+
+if __name__ == '__main__':
+    n = int(sys.stdin.readline())
+    dp = [0] * (n + 1)
+    dp[0], dp[1] = 0, 1
+
+    for i in range(2, n+1):
+        min_value = 1e9
+        for j in range(1, int(math.sqrt(i)) + 1):
+            min_value = min(min_value, dp[i - j ** 2])
+        dp[i] = min_value + 1
+    print(dp[n])
+    
