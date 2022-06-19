47794f620eddb4778094e542c037deb10333a115
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-06-20 00:28:53 +0900

    Update 5557.py

--- KBS/Dynamic Programming2/5557.py
+++ KBS/Dynamic Programming2/5557.py
@@ -1,23 +1,22 @@
-if __name__ == "__main__":
+import sys
+n = int(sys.stdin.readline().rstrip())
+arr = list(map(int,sys.stdin.readline().rstrip().split()))
 
-    N = int(input())  # 숫자의 개수
-    num_list = list(map(int, input().split()))  # 숫자 리스트
+# 0부터 20까지의 수만 가능
+dp = [[0]*21 for i in range(n)]
+# 첫번째 수에 대해 1로 초기화
+dp[0][arr[0]] = 1
 
-    # 2차원 dp 초기화
-    dp = []
-    for i in range(N - 1):
-        dp.append([0 for _ in range(21)])
+for i in range(1,n-1):
+    for j in range(21):
+        # 만약 전에 값이 있던 경우
+        if dp[i-1][j] != 0:
+            pre = j
+            nxt = arr[i]
+            # 조건에 부합하는 지 확인
+            if 0 <= pre + nxt <= 20:
+                dp[i][pre+nxt] += dp[i-1][pre]
+            if 0 <= pre - nxt <= 20:
+                dp[i][pre-nxt] += dp[i-1][pre]
 
-    dp[0][num_list[0]] = 1
-    for i in range(1, N - 1):
-        for j in range(21):
-            if dp[i - 1][j] != 0:
-                pre_value = j
-                next_value = num_list[i]
-                if 0 <= pre_value + next_value <= 20:
-                    dp[i][pre_value + next_value] += dp[i - 1][pre_value]
-                if 0 <= pre_value - next_value <= 20:
-                    dp[i][pre_value - next_value] += dp[i - 1][pre_value]
-
-    print(dp[N - 2][num_list[N - 1]])
-    
+print(dp[n-2][arr[n-1]])
