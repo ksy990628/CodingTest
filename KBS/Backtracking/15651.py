5fba1a56568e3d8eb2f4a12796051382b4933ea8
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-07-14 07:25:47 +0900

    Create 15651.py

---
+++ KBS/Backtracking/15651.py
@@ -0,0 +1,17 @@
+import sys
+n,m = map(int,sys.stdin.readline().strip().split())
+arr = [(i+1) for i in range(n)]
+
+def dfs(cnt,total):
+    if cnt == m:
+        print(*total)
+    else:
+        cnt += 1
+        for i in arr:
+            total.append(i)
+            dfs(cnt,total)
+            total.pop()
+        
+
+for i in arr:
+    dfs(1,[i])
