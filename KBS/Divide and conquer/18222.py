c36ad96e3290b5e1c8b937f5549ec36beab98ee1
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-07-15 09:00:44 +0900

    Update 18222.py

--- KBS/Divide and conquer/18222.py
+++ KBS/Divide and conquer/18222.py
@@ -1,4 +1,5 @@
 '''
+점화식 방법1
 to = 0
 t2n = tn
 t2n+1 = 1-tn
@@ -15,3 +16,30 @@ def recursive(n):
     else:
         return recursive(n//2)
 print(recursive(k-1))
+
+
+
+
+# 방법 2
+from sys import stdin
+from math import log
+
+
+def thue_morse(idx: int) -> int:
+    if idx == 0:
+        return 0
+
+    x = int(log(idx,2))  #float type
+
+    val = 0
+    while x > 0:
+        if idx >= 2 ** x:
+            val ^= 1
+            idx %= 2 ** x
+        x -= 1
+
+    return [val, val ^ 1][idx == 1]
+
+
+k = int(stdin.readline())
+print(thue_morse(k - 1))
