b71e931e3cc419d83cce74809d580d8440ae994d
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-07-21 20:30:40 +0900

    Update 20438.py

--- KBS/Prefix Sum/20438.py
+++ KBS/Prefix Sum/20438.py
@@ -20,7 +20,7 @@ for i in map(int, input().split()):
 prefix = [check[0]]
 # 각 번호 i까지 출석한 얘들 합 구하기
 for i in range(1, n+3):
-    prefix.append(prefix[-1]+check[i]) 
+    prefix.append(prefix[-1]+check[i])  #prefix[-1]은 가장 마지막 원소로, 현재 출석한 최대 인원을 나타냄. 즉 현재 출석한 최대 인원 + 다음 출석한 인원
 
 for _ in range(m):
     s, e = map(int, input().split()) # s부터 e까지 결석한 얘들
