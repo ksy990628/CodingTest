caee94dfa339780cbee4598a56ad3617c33ea024
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-07-21 20:29:20 +0900

    Create 20438.py

---
+++ KBS/Prefix Sum/20438.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n,k,q,m = map(int, input().split())
+
+sleep = [0]*(n+3) # 조는 학생, 졸고 있으면 1
+check = [0]*(n+3) # 출석한 학생, 출석 했으면 1
+
+# 조는 학생, 출석한 학생 검토
+for i in map(int, input().split()):
+    sleep[i] = 1
+for i in map(int, input().split()):
+    if sleep[i]: 
+        continue
+    for j in range(i, n+3, i):
+        if not sleep[j]:
+            check[j] = 1
+
+            
+prefix = [check[0]]
+# 각 번호 i까지 출석한 얘들 합 구하기
+for i in range(1, n+3):
+    prefix.append(prefix[-1]+check[i]) 
+
+for _ in range(m):
+    s, e = map(int, input().split()) # s부터 e까지 결석한 얘들
+    print(e-s+1 - (prefix[e]-prefix[s-1])) # 전체 - (s부터 e까지 출석한 얘들)
