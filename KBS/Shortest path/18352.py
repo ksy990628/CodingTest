baa74bdda7561bb2075a59c0c9b8bc335ec50ede
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-07-28 22:22:50 +0900

    Update 18352.py

--- KBS/Shortest path/18352.py
+++ KBS/Shortest path/18352.py
@@ -1,37 +1,34 @@
+import heapq
 import sys
-from collections import deque
-n, m, k,x = map(int,sys.stdin.readline().split())
+f = sys.stdin.readline
+INF = int(1e9)
 
-city = [[] for i in range(n+1)]
-for i in range(m):
-    a, b = map(int,sys.stdin.readline().split())
-    city[a].append(b)
-distance = [0]*(n+1)
-visited = [False]*(n+1)
+n, m, k, x = map(int, f().split())
+graph = [[] for _ in range(n+1)]
+distance = [INF] * (n+1)
 
+for _ in range(m):
+    a, b = map(int, f().split())
+    graph[a].append((b, 1))
 
-def bfs(x):
-    ans = []
-    q = deque([x])
-    visited[x] = True
-    distance[x] = 0
-
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
     while q:
-        now = q.popleft()
-        for i in city[now]:
-            if not visited[i]:
-                visited[i] =True
-            q.append(i)
-            distance[i] = distance[now] + 1
-            if distance[i] == k:
-                ans.append(i)
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist: continue
+        for j in graph[now]:
+            cost = dist + j[1]
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+                heapq.heappush(q, (cost, j[0]))
 
-    
-    if ans:
-        ans.sort()
-        for i in ans:
-            print(i)
-    else:
-        print(-1)
+dijkstra(x)
+answer = []
+for i in range(1, n+1):
+    if distance[i] == k: answer.append(i)
 
-bfs(x)
+if len(answer) == 0: print(-1)
+else:
+    for i in answer: print(i, end='\n')
