c3586f384da0369d0adfbcf14e178ac0fff157dd
김서영 <55133538+ksy990628@users.noreply.github.com>
2022-08-18 21:37:44 +0900

    Update 16398.py

--- KBS/Minimum Spanning Tree/16398.py
+++ KBS/Minimum Spanning Tree/16398.py
@@ -1,3 +1,4 @@
+# 내가 푼 Prim
 import sys
 import heapq
 input = sys.stdin.readline
@@ -24,3 +25,38 @@ while heap:
             heapq.heappush(heap, [edges[s-1][i],i+1])
 
 print(ans)
+
+
+# 영신이 Kruskal 코드
+import sys
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b): 
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n = int(sys.stdin.readline().strip().split()) 
+arr = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(n)]  
+parent = [i for i in range(n)] 
+edges = []  
+
+for i in range(1, n):
+    for j in range(i):
+        edges.append((arr[i][j], i, j))
+edges.sort()
+
+answer = 0
+for cost, a, b in edges: 
+    if find_parent(parent, a) != find_parent(parent, b): 
+        union(parent, a, b) 
+        answer += cost
+
+print(answer) 
